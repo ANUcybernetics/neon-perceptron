61761c844c39d7aa412bfd717e91660f2ab856b0
Ben Swift <ben@benswift.me>
2024-11-13 09:22:31 +1100

    remove template test code

--- lib/brainworms.ex
+++ lib/brainworms.ex
@@ -2,17 +2,4 @@ defmodule Brainworms do
   @moduledoc """
   Documentation for `Brainworms`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Brainworms.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
