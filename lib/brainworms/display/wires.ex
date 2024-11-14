4bb15443f8027615acdc328d69a11fc344cbec6d
Ben Swift <ben@benswift.me>
2024-11-15 09:16:35 +1100

    add Wires.breathe
    useful for demos

--- lib/brainworms/display/wires.ex
+++ lib/brainworms/display/wires.ex
@@ -16,4 +16,13 @@ defmodule Brainworms.Display.Wires do
     Circuits.SPI.transfer!(spi_bus, data)
     :ok
   end
+
+  def breathe(spi_bus) do
+    data =
+      Range.new(1, 24 * @pwm_controller_count)
+      |> Enum.map(fn x -> 0.5 + 0.5 * Utils.osc(0.1 * (1 + Integer.mod(x, 8))) end)
+      |> Utils.pwm_encode()
+
+    Circuits.SPI.transfer!(spi_bus, data)
+  end
 end
