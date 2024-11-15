b5e1279d4ef202aaa6c5787e6d44f384d661bc34
Ben Swift <ben@benswift.me>
2024-11-15 17:13:11 +1100

    move the activations/2 function to the model module

--- lib/brainworms/display/wires.ex
+++ lib/brainworms/display/wires.ex
@@ -25,30 +25,4 @@ defmodule Brainworms.Display.Wires do
 
     Circuits.SPI.transfer!(spi_bus, data)
   end
-
-  @doc """
-  Takes the current model state and (bitlist) input and returns a list of the intermediate
-  computations and final activations during inference. The list includes
-  element-wise multiplications and summed results for each layer, in order.
-
-  Used to map neural network calculations to wire brightness values for visualization.
-
-  The returned list is flattened with each element in [0.0, 1.0] and suitable for sending
-  to the PWM controllers.
-  """
-  def activations_to_brightness_list(model_state, input) do
-    weights = Map.get(model_state, :data)
-    %{"dense_0" => %{"kernel" => kernel_0}, "dense_1" => %{"kernel" => kernel_1}} = weights
-
-    dense_layers = [kernel_0, kernel_1]
-    input_vector = Nx.tensor(input, type: :f32)
-
-    Enum.reduce(dense_layers, {input_vector, []}, fn layer, {current_input, outputs} ->
-      intermediate = current_input |> Nx.new_axis(1) |> Nx.multiply(layer)
-      result = Nx.sum(intermediate, axes: [0])
-      {result, outputs ++ [intermediate, result]}
-    end)
-    |> elem(1)
-    |> Enum.flat_map(fn tensor -> Nx.to_flat_list(tensor) end)
-  end
 end
--- lib/brainworms/model.ex
+++ lib/brainworms/model.ex
@@ -115,4 +115,27 @@ defmodule Brainworms.Model do
     |> Nx.to_flat_list()
     |> List.first()
   end
+
+  @doc """
+  Takes the current model state and (bitlist) input and returns a list of the intermediate
+  computations and final activations during inference. The list includes
+  element-wise multiplications and summed results for each layer, in order.
+
+  Used to map neural network calculations to wire brightness values for visualization.
+  """
+  def activations(model_state, input) do
+    weights = Map.get(model_state, :data)
+    %{"dense_0" => %{"kernel" => kernel_0}, "dense_1" => %{"kernel" => kernel_1}} = weights
+
+    dense_layers = [kernel_0, kernel_1]
+    input_vector = Nx.tensor(input, type: :f32)
+
+    Enum.reduce(dense_layers, {input_vector, []}, fn layer, {current_input, outputs} ->
+      intermediate = current_input |> Nx.new_axis(1) |> Nx.multiply(layer)
+      result = Nx.sum(intermediate, axes: [0])
+      {result, outputs ++ [intermediate, result]}
+    end)
+    |> elem(1)
+    |> Enum.flat_map(fn tensor -> Nx.to_list(tensor) end)
+  end
 end
