6df6f174630434ed3ef0627ba020eadc469a4583
Ben Swift <ben@benswift.me>
2026-03-26 12:44:06 +1100

    update hex deps and add nx_eigen as RPi Nx backend
    Update all hex packages to latest within constraints. Add nx_eigen
    as the default Nx backend on the RPi target --- lighter than EXLA
    with precompiled aarch64 NIF. EXLA remains available as a fallback.

--- config/target.exs
+++ config/target.exs
@@ -1,5 +1,9 @@
 import Config
 
+# Use NxEigen as the default Nx backend on the target (lightweight, no XLA needed).
+# Switch to EXLA if you need GPU acceleration or hit missing op coverage.
+config :nx, default_backend: NxEigen.Backend
+
 # Use Ringlogger as the logger backend and remove :console.
 # See https://hexdocs.pm/ring_logger/readme.html for more information on
 # configuring ring_logger.
